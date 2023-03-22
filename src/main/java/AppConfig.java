956548a24c131b3e7508b2bfb5bcb13f17f34862
IsrapilKudashev <kudashev.020@gmail.com>
2023-03-23 00:29:18 +0300

    fix bean cat

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -13,7 +13,7 @@ public class AppConfig {
     }
 
     @Bean(name = "cat")
-    @Scope("Prototype")
+    @Scope("prototype")
     public Cat getCat() {
         Cat cat = new Cat();
         cat.setName("Черныш");
